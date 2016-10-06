aa4564476909886be5962b97770a49b1a3b7305e
Debika Sarkar <debikasarkar713@gmail.com>
2016-10-06 19:21:37 -0400

    js

---
+++ game.js
@@ -0,0 +1,29 @@
+
+
+
+
+function rainingDiamonds(){
+  var shineBright = $(), qt = 20;
+  for var i = 0; i < qt;
+}
+
+
+
+/*
+function movingDiamond(){
+var $diamond = $('#diamond');
+var distance = 1200;
+setInterval(function(){
+  $diamond.css('bottom', distance + 'px');
+  if(distance < -300){
+    distance = 1200;
+  } else {
+    distance -= 5;
+  }
+}, 10);
+};
+
+$(document).ready(function(){
+movingDiamond();
+})
+*/
