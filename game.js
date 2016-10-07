22397b576c73416c15cf655a60147607d475481e
Debika Sarkar <debikasarkar713@gmail.com>
2016-10-07 15:52:32 -0400

    stuff

--- game.js
+++ game.js
@@ -1,40 +1,77 @@
-function rainingDiamonds() {
-    var shineBright = $(),
-        qt = 20;
-    for (var i = 0; i < qt; ++i) {
-        var tiffanyDiamond = $('.diamond');
-        $tiffanyDiamond.css({
-            'top': (Math.random() * ('#store').width()) + 'px',
-            'left': (-Math.random() * ('#store').width()) + 'px'
+$(document).ready(function(){
 
-        });
-          var shineBright = $shineBright.add($tiffanyDiamond);
-    }
-    $('#store').prepend($tiffanyDiamond);
+
+});
+
+
+
+  /*var $Diamonds = $('.diamond');
+  var contain = $('.everything');
+
+setInterval(function() {
+   for(var i = 1; i < 4; i++) {
+      $Diamonds.add(CreateDiamonds());
+      MoveDiamonds();
+   }
+}, 2000);
+
+function CreateDiamonds(){
+   var $D =  $('</div>').addClass('.diamond');
+    .css(
+      top:Math.floor(Math.random()*450 ),
+
+
+    1000)
+
+   .appendTo(contain);
+
+
+
+}
+
+function MoveDiamonds() {
+   $Diamonds.each(function() {
+      var x = Math.floor(Math.random() * 990);
+      var y = Math.floor(Math.random() * 560);
+
+      $('div').animate({"left": x + "px"}, "slow");
+      $('div').animate({"top": y + "px"}, "slow");
+   })
 }
 
 
+//         var $shineBright = $(), qt = 20;
+
+//         for (var i = 0; i < qt; ++i) {
+//             var $diamond = $('<div id="diamond"></div>');
+//             $diamond.css({
+//                 'left': (Math.random() * $('#site').width()) + 'px',
+//                 'top': (- Math.random() * $('#site').height()) + 'px'
+//             });
+//
+//             $shineBright = $shineBright.add($diamond);
+//         }
+//         $('#store').prepend($shineBright);
+
+//         $shineBright.animate({
+//             top: "500px",
+//             opacity : "0",
+//         }, Math.random() + 5000, function(){
+//             $(this).remove();
+//             if (--qt < 1) {
+//                 rainingDiamonds();
+//             }
+//         });
+  //   }
+//     rainingDiamonds();
+});
 
 
 
 
-/*
-This was to check if it was moving.
 
-function movingDiamond(){
-var $diamond = $('#diamond');
-var distance = 1200;
-setInterval(function(){
-  $diamond.css('bottom', distance + 'px');
-  if(distance < -300){
-    distance = 1200;
-  } else {
-    distance -= 5;
-  }
-}, 10);
-};
 
-$(document).ready(function(){
-movingDiamond();
-})
-*/
+
+
+
+
