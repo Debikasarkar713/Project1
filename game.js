99d4c3d7a27cc15665c04b543910b3f6a135c76f
Debika Sarkar <debikasarkar713@gmail.com>
2016-10-08 21:34:55 -0400

    continue

--- game.js
+++ game.js
@@ -1,77 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
+
+   /* runThrough();
+
+    function runThrough() {
+
+
+
+      for (var i = 0; i < .length ; i++) {
+
+      }
+
+        }
+*/
+
+
+
+    fallingDiamond();
+
+
+
+    function fallingDiamond() {
+
+        var stop = setInterval(function() {
+            $('.diamond').css({ 'top': '+=10px' });
+            console.log($('#flex-container').innerHeight())
+            if ($('.diamond').offset().top > $('#flex-container').innerHeight()) {
+                clearInterval(stop);
+            }
+        }, 100);
+
+    }
 });
-
-
-
-  /*var $Diamonds = $('.diamond');
-  var contain = $('.everything');
-
-setInterval(function() {
-   for(var i = 1; i < 4; i++) {
-      $Diamonds.add(CreateDiamonds());
-      MoveDiamonds();
-   }
-}, 2000);
-
-function CreateDiamonds(){
-   var $D =  $('</div>').addClass('.diamond');
-    .css(
-      top:Math.floor(Math.random()*450 ),
-
-
-    1000)
-
-   .appendTo(contain);
-
-
-
-}
-
-function MoveDiamonds() {
-   $Diamonds.each(function() {
-      var x = Math.floor(Math.random() * 990);
-      var y = Math.floor(Math.random() * 560);
-
-      $('div').animate({"left": x + "px"}, "slow");
-      $('div').animate({"top": y + "px"}, "slow");
-   })
-}
-
-
-//         var $shineBright = $(), qt = 20;
-
-//         for (var i = 0; i < qt; ++i) {
-//             var $diamond = $('<div id="diamond"></div>');
-//             $diamond.css({
-//                 'left': (Math.random() * $('#site').width()) + 'px',
-//                 'top': (- Math.random() * $('#site').height()) + 'px'
-//             });
-//
-//             $shineBright = $shineBright.add($diamond);
-//         }
-//         $('#store').prepend($shineBright);
-
-//         $shineBright.animate({
-//             top: "500px",
-//             opacity : "0",
-//         }, Math.random() + 5000, function(){
-//             $(this).remove();
-//             if (--qt < 1) {
-//                 rainingDiamonds();
-//             }
-//         });
-  //   }
-//     rainingDiamonds();
-});
-
-
-
-
-
-
-
-
-
-
